5847e6bb5a817d1004330af9d51377e8f7110665
John C Hamel <JHAMEL@amfam.com>
2019-05-24 17:19:14 -0500

    Create typeDefs, resolvers, and add first endpoints

--- hello-world-server/server.js
+++ hello-world-server/server.js
@@ -1,8 +1,28 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const { graphqlExpress, graphiqlExpress }= require('apollo-server-express');
+const { makeExecutableSchema } = require('graphql-tools');
 
 const port = 9009;
+
+const typeDefs = `
+	type Query {
+		greeting: String
+	}
+`;
+
+const resolvers = {
+	Query: {
+		greeting: () => 'Hello World!'
+	}
+}
+
+const schema = makeExecutableSchema({typeDefs, resolvers});
+
 const app = express();
+
 app.use(bodyParser.json());
+app.use('/graphql', graphqlExpress({schema}));
+app.use('/graphiql', graphiqlExpress({endpointURL: 'graphql'}));
 app.listen(port, () => console.log(`Server running on port ${port}`));
